8c853a19542f24420868e80e8c0e47557214e2ce
Laura Humphries <Laura@lauras-mbp.attlocal.net>
2016-05-08 23:30:57 -0500

    fixed bodyparser and express-session warnings

--- app.js
+++ app.js
@@ -71,7 +71,8 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -100,6 +101,7 @@ models.sequelize.sync().then(function() {
  */
 
 app.use(session({
+  saveUninitialized: true,
   resave: false, 
   rolling: true,
   secret: 'sumo cream!'
